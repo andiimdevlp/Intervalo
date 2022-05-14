546ece4f43f9efb521696636d345a43058cd9daf
Anderson Nascimento <dev.cs.anderson@gmail.com>
2022-05-14 20:24:52 -0300

    Adicionar arquivos de projeto.

---
+++ Ponto/MarcaPonto.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ponto
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MarcaPonto : ContentPage
+    {
+        public MarcaPonto()
+        {
+            InitializeComponent();
+            btnCalcular.Clicked += BtnCalcular_Clicked;
+        }
+
+        private void BtnCalcular_Clicked(object sender, EventArgs e)
+        {
+            TimeSpan intervalo = new TimeSpan(01, 30, 00);
+            lblRetorno.Text = "Você deve voltar ao trabalho\n" + DateTime.Parse(etrSaida.Text).Add(intervalo);
+        }
+    }
+}
