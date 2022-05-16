439da8b9505c419b4f82a70f492f429dda7a8828
Anderson Nascimento <dev.cs.anderson@gmail.com>
2022-05-15 21:36:46 -0300

    Notificação local adicionada icon alterado

--- Ponto/MarcaPonto.xaml.cs
+++ Ponto/MarcaPonto.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System;
-
+﻿using Plugin.LocalNotification;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,11 +13,39 @@ namespace Ponto
             InitializeComponent();
             btnCalcular.Clicked += BtnCalcular_Clicked;
         }
-
         private void BtnCalcular_Clicked(object sender, EventArgs e)
         {
-            TimeSpan intervalo = new TimeSpan(01, 30, 00);
-            lblRetorno.Text = "Você deve voltar ao trabalho\n" + DateTime.Parse(etrSaida.Text).Add(intervalo);
+            lblRetorno.Text = "Você deve voltar ao trabalho\n" + DateTime.Parse(etrSaida.Text).AddMinutes(90);
+
+            var notificacao = new NotificationRequest
+            {
+                BadgeNumber = 1,
+                Description = "Seu horario de almoço acabou!",
+                Sound = "soundNotification.wav",
+                Title = "Anna - Alerta!",
+                ReturningData = "Dummy Data",
+                NotificationId = 1337,
+                Image =
+                {
+                    ResourceName = "icon.png"
+                },
+                Schedule =
+                {
+                   NotifyTime = DateTime.Parse(etrSaida.Text).AddMinutes(80),
+                   RepeatType = NotificationRepeat.TimeInterval,
+                   NotifyRepeatInterval = TimeSpan.FromSeconds(30)
+                },
+                Android = new Plugin.LocalNotification.AndroidOption.AndroidOptions
+                {
+                    AutoCancel = false,
+                    Priority = Plugin.LocalNotification.AndroidOption.AndroidPriority.High,
+                    IconSmallName =
+                    {
+                        ResourceName = "icon.png"
+                    }
+                }
+            };
+            NotificationCenter.Current.Show(notificacao);
         }
     }
 }
